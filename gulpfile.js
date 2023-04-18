056f30ad6a89d75555e57232c910b6e2d7aca9db
flamminger <101122248+flamminger@users.noreply.github.com>
2023-04-18 17:45:50 +0200

    update package.json, gulpfile, add base scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
+const gulp = require('gulp');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('css', function () {
-    var sass = require('gulp-sass');
-    var postcss = require('gulp-postcss');
-    var autoprefixer = require('autoprefixer');
-
+function buildStyles() {
     return gulp.src('./asset/sass/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed',
-            includePaths: ['node_modules/susy/sass']
-        }).on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
             autoprefixer()
         ]))
         .pipe(gulp.dest('./asset/css'));
-});
+};
 
-gulp.task('css:watch', function () {
-    gulp.watch('./asset/sass/*.scss', gulp.series('css'));
-});
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+    gulp.watch('./asset/sass/*.scss', buildStyles());
+};
